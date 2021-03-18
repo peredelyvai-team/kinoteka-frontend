8bbd55f0a3393608cc690d32cd10f4370c4af450
Leonov <mbdoy213@gmail.com>
2021-03-19 00:43:24 +0300

    Фикс верстки

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,16 +3,16 @@ import styles from './Card.module.css'
 import cn from 'classnames'
 
 export const Card = props => {
-  const { id, image, title, overwiew } = props
+  const { id, image, title, overview } = props
 
   return (
     <div className={cn(styles.cardWrapper)}>
-      <div className={cn(styles.cardFront, 'card')}>
+      <div className={cn(styles.cardFront)}>
         <img src={image} alt='poster' className={styles.cardPoster} />
         <span className={styles.cardTitle}>{title}</span>
       </div>
-      <div className={cn(styles.cardBack, 'card')}>
-        <p className={cn(styles.cardOverwiev, 'card-text')}>{overwiew}</p>
+      <div className={cn(styles.cardBack)}>
+        <p className={cn(styles.cardOverwiev, 'card-text')}>{overview}</p>
       </div>
     </div>
   )
