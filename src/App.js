a4bd6619afb956cf45c8cd55f07da83d4f9026d7
Leonov <mbdoy213@gmail.com>
2021-03-18 23:01:58 +0300

    Решает конфликт #2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import { useEffect } from 'react'
 import cn from 'classnames'
 import { useSelector } from 'react-redux'
-<<<<<<< HEAD
-import './bootstrap.min.css'
-import { Card } from './components/Card'
-=======
 import { Route, Redirect } from 'react-router-dom'
 import { useAction } from './hooks'
 import { Auth, Main } from './modules'
 import { NavBar } from './components'
 import './bootstrap.min.css'
 
->>>>>>> f36ac31bb19c586dddfad460070cd71f8eac7ce7
 function App() {
   const { isAuth } = useSelector(state => state.app)
   const { setAuth } = useAction()
@@ -21,14 +16,10 @@ function App() {
 
   return (
     <div className={cn('min-vh-100')}>
-<<<<<<< HEAD
-      <Card />
-=======
       <NavBar />
       <Route exact path='/'>
         <Main />
       </Route>
->>>>>>> f36ac31bb19c586dddfad460070cd71f8eac7ce7
       {isAuth && <Redirect to='/' from='/auth' />}
       <Route path='/auth' component={Auth} />
     </div>
