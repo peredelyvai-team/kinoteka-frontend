5a7f4ac261138007fdfe73751abcc67f2d23f175
Leonov <mbdoy213@gmail.com>
2021-03-25 01:30:51 +0300

    Фикс иконок в main, когда пользователь не авторизовался

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsEye, BsEyeFill } from 'react-icons/bs'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useSelector } from 'react-redux'
 import { Rating } from '../../components'
 import cn from 'classnames'
 import styles from './Card.module.css'
 
 export const Card = props => {
   const { id, image, rating, type, title, isViewed, isFavorite } = props
+  const { isAuth } = useSelector(state => state.app)
   return (
     <Link
       to={{ pathname: `/item/${id}`, state: { id, type } }}
@@ -16,16 +18,18 @@ export const Card = props => {
       <div className={cn(styles.cardWrapper)}>
         <img src={image} alt='poster' className={styles.cardPoster} />
         <Rating rating={rating} size='xs' className={styles.cardRating} />
-        {isViewed ? (
-          <BsEyeFill className={styles.iconView} />
-        ) : (
-          <BsEye className={styles.iconView} />
-        )}
-        {isFavorite ? (
-          <MdFavorite className={styles.iconFavorite} />
-        ) : (
-          <MdFavoriteBorder className={styles.iconFavorite} />
-        )}
+        {isAuth &&
+          (isViewed ? (
+            <BsEyeFill className={styles.iconView} />
+          ) : (
+            <BsEye className={styles.iconView} />
+          ))(
+            isFavorite ? (
+              <MdFavorite className={styles.iconFavorite} />
+            ) : (
+              <MdFavoriteBorder className={styles.iconFavorite} />
+            )
+          )}
         <span className={styles.cardTitle}>{title}</span>
       </div>
     </Link>
